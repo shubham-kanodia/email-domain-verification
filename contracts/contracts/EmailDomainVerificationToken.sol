676ef31cdaa82eaae7bd63fc4c3628e147a54b46
shubham-kanodia <shubhamkanodia123@gmail.com>
2022-12-13 15:07:44 +0530

    Modify contract to mint SVG NFTs

--- contracts/contracts/EmailDomainVerificationToken.sol
+++ contracts/contracts/EmailDomainVerificationToken.sol
@@ -2,14 +2,15 @@
 pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-contract EmailDomainVerificationToken is ERC721URIStorage, EIP712, AccessControl {
+contract EmailDomainVerificationToken is ERC721Enumerable, EIP712, AccessControl {
     using Strings for uint256;
 
     uint256 tokenId;
+    mapping (uint256 => string) public emailDomains;
 
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     string baseURI = "";
@@ -39,7 +40,7 @@ contract EmailDomainVerificationToken is ERC721URIStorage, EIP712, AccessControl
     {
         require(_verify(_hash(account, emailDomain), signature), "Invalid signature");
         _safeMint(account, tokenId);
-        _setTokenURI(tokenId, emailDomain);
+        emailDomains[tokenId] = emailDomain;
     }
 
     function _hash(address account, string memory emailDomain)
@@ -61,5 +62,19 @@ contract EmailDomainVerificationToken is ERC721URIStorage, EIP712, AccessControl
     function recover(address account, string memory emailDomain, bytes memory signature) public view returns (address) {
         bytes32 digest = _hash(account, emailDomain);
         return ECDSA.recover(digest, signature);
+    }
+
+    function tokenURI(uint256 tokenId) override public view returns (string memory) {
+        string[19] memory parts;
+        parts[0] = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 100 100"><g><rect x="0" y="0" width="100" height="100" fill="red"></rect><text x="10" y="50" font-family="Verdana" font-size="10" fill="blue">@';
+        parts[1] = emailDomains[tokenId];
+        parts[2] = '</text></g></svg>';
+
+        string memory output = string(abi.encodePacked(parts[0], parts[1], parts[2]));
+        
+        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "Token #', Strings.toString(tokenId), '", "description": "Email domain verification NFT", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(output)), '"}'))));
+        output = string(abi.encodePacked('data:application/json;base64,', json));
+
+        return output;
     }
 }
